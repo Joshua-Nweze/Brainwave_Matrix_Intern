20257180bdf81d29dbe2d5a89ca6167a6c30c575
Joshua-Nweze <joshuanweze270@gmail.com>
2024-06-28 01:46:21 +0100

    chore: update gitignore

--- Task_Two/backend/dist/routes/unAuthUser.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import unAuthUserController from '../controllers/unAuthUser.controller.js';
-let { getBlogs, getBlog } = unAuthUserController;
-let router = express.Router();
-router.get('/get-blogs', (req, res) => getBlogs(req, res));
-router.get('/get-blog', (req, res) => getBlog(req, res));
-export default router;
-//# sourceMappingURL=unAuthUser.routes.js.map
