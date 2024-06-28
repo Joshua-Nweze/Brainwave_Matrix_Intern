3ace9e3e0179f8465a6e3672aa6690912658e8dc
Joshua-Nweze <joshuanweze270@gmail.com>
2024-06-28 08:51:30 +0100

    update cors

--- Task_Two/backend/dist/index.js
+++ Task_Two/backend/dist/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'veeblog.netlify.app'],
     credentials: true
 }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
